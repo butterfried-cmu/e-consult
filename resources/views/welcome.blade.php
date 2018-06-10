df39fdff06b1395a2ea355cb31491c093f92de19
nira <nayzaa2010@hotmail.com>
2018-06-10 13:20:22 +0700

    project starter

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css')}}">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content" id="app">
+                <div class="title m-b-md">
+                    <example-component></example-component>
+                </div>
+            </div>
+        </div>
+        <script type="text/javascript" src="{{ asset('js/app.js')}}"></script>
+    </body>
+</html>
