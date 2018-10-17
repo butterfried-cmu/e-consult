79e5aa84e2bb65e7bd7c504154aca072ad4609aa
ploychanok <plo_y@live.com>
2018-10-17 18:21:18 +0700

    update print file

--- resources/views/consultFormTemplate.blade.php
+++ resources/views/consultFormTemplate.blade.php
@@ -1,19 +1,158 @@
-<h1>Customer List</h1>
-<table>
-    <thead>
-    <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Phone</th>
-    </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>id</td>
-            <td>name</td>
-            <td>email</td>
-            <td>phone</td>
-        </tr>
-    </tbody>
-</table>
+<style>
+.table {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+
+th,td {
+  padding: 5px 10px;
+}
+.center {
+  text-align: center;
+}
+.right {
+  text-align: right;
+}
+.left {
+  text-align: left;
+}
+#print {
+  margin: 0 30px;
+}
+h2 {
+  margin-bottom: -10px
+}
+</style>
+
+<div id="print">
+        <div class="center">
+          <br/>
+          <h3><b>Doctor's Order Sheet Kokha Hospital</b></h3>
+        </div>
+        <div class="right">
+          TimeStamp
+        </div>
+        <br/>
+      <div>
+        <table class="table">
+          <tr class="table">
+            <!-- Stamp Date -->
+            <th class="table">Admissiton Date</th>
+            <!-- Stamp Time -->
+            <th class="table">Time</th>
+            <!-- Miss,Mrs,Mr -->
+            <th class="table">Name title</th>
+            <th class="table">Name</th>
+            <th class="table">Date of Birth</th>
+            <th class="table">Address</th>
+          </tr>
+          <tr class="table">
+            <td class="center table">13/12/2018</td>
+            <td class="center table">hh:mm</td>
+            <td class="center table">Mr.</td>
+            <td class="table">Sammy Read</td>
+            <td class="center table">13/12/1995</td>
+            <td class="table">123/6 moo3 Banndu Mueang Lampang 57100</td>
+          </tr>
+        </table>
+      </div>
+      <br/>
+      <div>
+          <table>
+              <tr>
+                  <td>Dx:</td>
+                  <td>DM</td>
+              </tr>
+          </table>
+          <table>
+              <tr>
+                  <td>BW:</td>
+                  <td>65</td>
+          
+                  <td>BMI:</td>
+                  <td>26.6</td>
+          
+                  <td>T</td>
+                  <td>37</td>
+          
+                  <td>FBS:</td>
+                  <td>100</td>
+          
+                  <td>Cr:</td>
+                  <td>0.43</td>
+          
+                  <td>Clearance:</td>
+                  <td>122.1</td>
+          
+                  <td>Stage:</td>
+                  <td>1</td>
+              </tr>
+            </table>
+      </div>
+      <hr/>
+        <div>
+            <table>
+                <tr>
+                    <td>Hx: 10/10/2018</td>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                    <td>FBS:</td>
+                    <td>100</td>
+                
+                    <td>BP1</td>
+                    <td>200/90</td>
+                
+                    <td>BP2:</td>
+                    <td>110/90</td>
+                
+                    <td>P:</td>
+                    <td>120</td>
+                </tr>
+              </table>
+              <table>
+                  <tr>
+                      <td>Hx: 11/10/2018</td>
+                  </tr>
+              </table>
+              <table>
+                  <tr>
+                      <td>FBS:</td>
+                      <td>100</td>
+                  
+                      <td>BP1</td>
+                      <td>155/90</td>
+                  
+                      <td>BP2:</td>
+                      <td>110/90</td>
+                  
+                      <td>P:</td>
+                      <td>80</td>
+                  </tr>
+                </table>
+                <hr/>
+                <table>
+                  <tr>
+                    <td>Chief Complain	</td>
+                    <td>FBS >= 130 mg% 2 visit</td>
+                  </tr>
+            
+                  <tr>
+                    <td>Assessment and plan</td>
+                    <td>MFM 1x2 0pc, Slmvas 1209 ½ x ns</td>
+                  </tr>
+                  <tr>
+                    <td>Consult Order</td>
+                    <td>Ena (5) 1x20 pc</td>
+                  </tr>
+                  <tr>
+                      <td>Notes</td>
+                      <td>-</td>
+                  </tr>
+                </table>
+        </div>
+        <div class="right">
+            Name of Doctor Here
+        </div>
+</div>
