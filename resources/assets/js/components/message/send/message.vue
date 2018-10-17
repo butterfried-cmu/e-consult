986121665a679adf95540ff994fca4be1cc1570a
ploychanok <plo_y@live.com>
2018-10-18 02:02:15 +0700

    vue-simple-lightbox

--- resources/assets/js/components/message/send/message.vue
+++ resources/assets/js/components/message/send/message.vue
@@ -1,15 +1,39 @@
 <script>
     import axios from 'axios';
+    import Lightbox from 'vue-simple-lightbox'
 
     export default {
         template: require('./message.html'),
-
+        components: {
+            Lightbox
+        },
         data() {
             return {
                 isMounted: false,
                 message: "",
                 upload_files: [],
-                upload_files_extension: []
+                upload_files_extension: [],
+                images : [
+            {
+                src : 'https://cdn.rawgit.com/vrajroham/vrajroham.github.io/85d64ac5/imgs/img1.jpg',
+                title : 'Image 2'
+            },
+            {
+                src : 'https://cdn.rawgit.com/vrajroham/vrajroham.github.io/85d64ac5/imgs/img2.jpg',
+                title : 'Image 3'
+            },
+            {
+                src : 'https://cdn.rawgit.com/vrajroham/vrajroham.github.io/85d64ac5/imgs/img3.jpg',
+                title : ''
+            },
+            {
+                src : 'https://cdn.rawgit.com/vrajroham/vrajroham.github.io/85d64ac5/imgs/img4.jpg',
+                title : ''
+            },
+          ],
+          options : {
+            closeText : 'x'
+          }
             }
         },
 
